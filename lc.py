df19a5ddf415a43395e25e1bc01896642036a269
Zhen Qiao <qiaozhenchina@gmail.com>
2022-08-30 01:19:41 -0700

    fix dateDelta bug and add new member

--- lc.py
+++ lc.py
@@ -16,7 +16,7 @@ def open_url(username, limit):
 
 def get_prev_week_date():
     today = datetime.date.today()
-    prev_monday = today - datetime.timedelta(days=-today.weekday(), weeks=1)
+    prev_monday = today - datetime.timedelta(days=today.weekday(), weeks=1)
     prev_sunday = prev_monday + datetime.timedelta(days=6)
     return (prev_monday, prev_sunday)
 
@@ -24,7 +24,6 @@ def is_submited_in_prev_week(ts):
     dates = get_prev_week_date()
     ts_date = datetime.datetime.fromtimestamp(int(ts)).date()
     return dates[0] <= ts_date <= dates[1]
-
 
 def gen_submission_list(html_str):
     obj = json.loads(html_str)
@@ -35,40 +34,36 @@ def gen_submission_list(html_str):
     sub_lst = obj['data']['recentAcSubmissionList']
     if not sub_lst:
         return [-1]
-    ac_problems = []
+    prev_ac_problems = []
     for item in sub_lst:
-        if not is_submited_in_prev_week(item['timestamp']):
-            continue
-        ac_problems.append(item['title'])
-    return ac_problems
+        if is_submited_in_prev_week(item['timestamp']):
+            prev_ac_problems.append(item['title'])
+    return prev_ac_problems
 
 
 def print_result(username, ac_problems):
-    ac_limit = 10
-    new_problem_limit = 3
-    unit_price = 10
-    usd2rmb = 7
-    new_problems_count = len(set(ac_problems))
-    ac_count = len(ac_problems)
-    if ac_count == 1 and ac_problems[0] == -1:
+    prev_ac_count = len(ac_problems)
+    if prev_ac_count == 1 and ac_problems[0] == -1:
         res = username + '\tInvalid'
         print(res)
         return
-    res = username + '\ttotal AC: ' + str(ac_count)
-    if ac_count >= ac_limit and new_problems_count < new_problem_limit:
-        res += '\tOnly ' + str(new_problems_count) + ' are new problems"""  """'
-    if ac_count < ac_limit:
-        rmb = unit_price * (ac_limit - ac_count)
-        res += '\t Penalty: ' + \
-            str(rmb) + ' RMB($' + '{:.2f}'.format(rmb / usd2rmb) + ')'
+    ac_limit = 10
+    unit_price = 10
+    usd2rmb = 7
+    res = username + '\tPrev week AC: ' + str(prev_ac_count)
+    if prev_ac_count < ac_limit:
+        rmb = unit_price * (ac_limit - prev_ac_count)
+        res += '  \tPenalty: ¥' + \
+            str(rmb) + '($' + '{:.2f}'.format(rmb / usd2rmb) + ')'
     print(res)
 
 
 name_dict = {'QiaoZhennn': 'Zhen',
+             'pinisthn': 'Nicole',
              'RachelLiu66': 'Rachel',
              'SeSmC06': 'Peng',
              'ychen01': 'Yian'}
-limit = 15
+limit = 20
 
 if __name__ == '__main__':
     dates = get_prev_week_date()
